fa359d295d365887f7fb10864c41a6525ca8aeed
Kiwi <42833050+Kiwi233@users.noreply.github.com>
2025-12-18 12:10:16 +0800

    Update action.py

--- src/gtnh_translation_compare/cmd/action.py
+++ src/gtnh_translation_compare/cmd/action.py
@@ -28,7 +28,7 @@ class Action:
             client=httpx.AsyncClient(
                 headers={"Authorization": paratranz_token},
                 base_url="https://paratranz.cn/api",
-                timeout=120,
+                timeout=600,
             ),
             project_id=paratranz_project_id,
             cache_dir=settings.PARATRANZ_CACHE_DIR,
